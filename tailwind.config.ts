da3b68889068f0e5e0f4c9f47136793835c3ba0d
Jeremy Kohn <5316595+jeremykohn@users.noreply.github.com>
2026-01-12 19:13:59 -0600

    feat: Add config files

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    './app/**/*.{js,ts,vue}',
+    './components/**/*.{js,ts,vue}',
+    './layouts/**/*.{js,ts,vue}',
+    './pages/**/*.{js,ts,vue}',
+    './plugins/**/*.{js,ts,vue}',
+    './shared/**/*.{js,ts,vue}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
